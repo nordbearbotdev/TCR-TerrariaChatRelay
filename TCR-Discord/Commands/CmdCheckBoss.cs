e9984f640c136a0e871329fff03a243d76346780
WoodMan <85753549+nordbearbotdev@users.noreply.github.com>
2022-05-13 16:07:51 +0300

    Update CmdCheckBoss.cs

--- TCR-Discord/Commands/CmdCheckBoss.cs
+++ TCR-Discord/Commands/CmdCheckBoss.cs
@@ -1,1 +1,10 @@
-.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCRDiscord;
+using TerrariaChatRelay;
+using TerrariaChatRelay.Command;
+
+namespace TCRDiscord.Commands
