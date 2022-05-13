3d43897c1274fed8614a2b85490009f578ed2123
WoodMan <85753549+nordbearbotdev@users.noreply.github.com>
2022-05-13 16:09:14 +0300

    Update CmdCheckBoss.cs

--- TCR-Discord/Commands/CmdCheckBoss.cs
+++ TCR-Discord/Commands/CmdCheckBoss.cs
@@ -6,5 +6,30 @@ using System.Threading.Tasks;
 using TCRDiscord;
 using TerrariaChatRelay;
 using TerrariaChatRelay.Command;
+using Terraria.ModLoader;
 
 namespace TCRDiscord.Commands
+{
+	internal class BossProgressionCommand : ModCommand
+	{
+		public override CommandType Type => CommandType.Console;
+
+		public override string Command => "BossProgress";
+
+		public override string Description => "View Boss Progression";
+
+		// public override string Usage => base.Usage;
+
+		public override void Action(CommandCaller caller, string input, string[] args) {
+			var sortedBosses = BossChecklist.bossTracker.SortedBosses.OrderBy(x => x.progression);
+			foreach (var boss in sortedBosses) {
+				bool downed = boss.downed();
+				Console.ForegroundColor = downed ? ConsoleColor.Green : ConsoleColor.Yellow;
+				Console.WriteLine($"{boss.type}: {boss.DisplayName}");
+			}
+			Console.ResetColor();
+		}
+	}
+}
+
+
